0c0658d5f899d27f21af211c3e51eae86f6c02b3
clwest <westremarketing@gmail.com>
2020-10-07 12:33:16 -0600

    made header a footer

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("sanity check")
+
+//Basic game operations
+
+const game = {
+    counter: 0,
+    colorArrary: ["blue", "green", "yellow", "red"],
+    currentGame: [],
+    player: [],
+
+}
+/*
+newGame = () {
+    for (let i = 0; i < colorArrary.length; i++){
+
+    }
+
+}
+*/
