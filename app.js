12aa50ff4896aa75ac83993e24810f06b222dc2d
clwest <westremarketing@gmail.com>
2020-10-07 13:56:41 -0600

    buttons are linked and clickable kinda

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ console.log("sanity check")
 
 //Basic game operations
 
-const game = {
-    counter: 0,
-    colorArrary: ["blue", "green", "yellow", "red"],
-    currentGame: [],
-    player: [],
-
-}
-/*
-newGame = () {
-    for (let i = 0; i < colorArrary.length; i++){
-
-    }
-
-}
-*/
+let score = 0
+let colorArrary = ["blue", "green", "yellow", "red"]
+let currentGame = []
+let player = []
+const greenButton = document.getElementById("green").addEventListener("click", function(){
+    console.log("I Clicked the green button")
+})
+const redButton = document.getElementById("red").addEventListener("click", function(){
+    console.log("I clicked the red button")
+})
+const blueButton = document.getElementById("blue").addEventListener("click", function(){
+    console.log("I clicked the blue button")
+})
+const yellowButton = document.getElementById("yellow").addEventListener("click", function(){
+    console.log("I clicked the yellow button")
+})
+const startButton = document.getElementById("start").addEventListener("click", function(){
+    console.log("Let the games begin")
+})
