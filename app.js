f6eb919ebbf158c038f001d532c0b0a4bc032af5
clwest <westremarketing@gmail.com>
2020-10-11 22:25:36 -0600

    still not working as intended but its getting closer

--- app.js
+++ app.js
@@ -4,70 +4,81 @@ console.log("sanity check")
 let currentGame = []
 let playerChoice = []
 let flash = []
-let turn;
+let turn; 
 let winning;
-let win;
-let ready = false
-let simon;
-let startGame = false
+let gameIsStarted = false
 let intervalId;
-let scoreCounter = document.getElementById("score-counter")
+let scoreNumber = document.getElementById("score-number")
+let allColorFlash = 0
+let allColorFlashRef;
 
-const simonStart = document.getElementById("simon").addEventListener("click", function() {
-    if (simonStart.click === true) {
-        startGame = true
-        scoreCounter.innerHTML = 0
-    }else 
-    startGame = false
-    scoreCounter.innerHTML = ""
-    clearColor()
-    clearInterval(intervalId)
+
+const startButton = document.getElementById("start")
+const blueButton = document.getElementById("blue")
+const greenButton = document.getElementById("green")
+const yellowButton = document.getElementById("yellow")
+const redButton = document.getElementById("red")
+
+
+
+startButton.addEventListener("click", function(){
+    setupGame()
 })
 
-const startButton = document.getElementById("start").addEventListener("click", function(){
-    if (startGame || win) {
-        player()
-    }
-    player()
-})
-
-player = () => {
+setupGame = () => {
+    allColorFlash = 0
     currentGame = []
     playerChoice = []
-    turn = 1
+    turn = 0
     flash = 0
-    winning = true
     intervalId = 0
-    scoreCounter.innerHTML = 0
-    win = false
+    scoreNumber.innerHTML = '0'
     playerSelect = true
     for (let i = 0; i < 10; i++) {
         currentGame.push(Math.floor(Math.random() * 4) + 1)
     }
     simon = true
 
-    intervalId = setInterval(game, 1000)
+    //intervalId = setInterval(game, 200)
+    game();
 }
 game = () => {
-    ready = false
-    if (flash === turn) {
-        clearInterval(intervalId)
-        simon = false;
+    if (simon) {
         clearColor()
-        scoreCounter.innerHtml = 0
-        ready = true
-    }
-        if (simon) {
-        clearColor()
-        setTimeout(() => {
-            if (currentGame[flash] === 1) blueFlash()
-            if (currentGame[flash] === 2) greenFlash()
-            if (currentGame[flash] === 3) yellowFlash()
-            if (currentGame[flash === 4]) redFlash()
-            flash++
+
+        allColorFlashRef = setInterval(() => {
+            if (allColorFlash === 1) blueFlash()
+            if (allColorFlash === 2) greenFlash()
+            if (allColorFlash === 3) yellowFlash()
+            if (allColorFlash === 4) redFlash()
+            if (allColorFlash > 4) {
+                clearTimeout(allColorFlashRef)
+                clearColor()
+                flashTurn()
+                return;
+            }
+            allColorFlash++
         }, 200)
     }
 }
+
+flashColor = (color) => {
+    console.log("flashing color", color)
+    if (color === 1) blueFlash()
+    if (color === 2) greenFlash()
+    if (color === 3) yellowFlash()
+    if (color === 4) redFlash()
+}
+
+flashTurn = () => {
+    setTimeout(() => {
+        flashColor(currentGame[turn])
+        setTimeout(() => {
+            clearColor()
+        }, 200)
+    }, 500)
+}
+
 blueFlash = () => {
     if (blue) { 
         click = true
@@ -103,85 +114,80 @@ clearColor = () => {
     red.style.backgroundColor = "rgba(221, 75, 62, 0.8)"
 }
 
-
-const blueButton = document.getElementById("blue").addEventListener("click", function() {
-    if (ready) {
-        playerChoice.push(1)
-        playerWin()
-        blueFlash()
-        if(!win) {
-            setTimeout(() => {
-                clearColor
-            }, 300)
-        }
-    }
-})
-
-
-
-
-const greenButton = document.getElementById("green").addEventListener("click", function(){
-    if (ready) {
-        playerChoice.push(2)
-        playerWin()
-        greenFlash()
-        if(!win) {
-            setTimeout(() => {
-                clearColor
-            }, 300)
-        }
-    }
-})
-const yellowButton = document.getElementById("yellow").addEventListener("click", function() {
-    if (ready) {
-        playerChoice.push(3)
-        Playerwin()
-        yellowFlash()
-        if(!win) {
-            setTimeout(() => {
-                clearColor
-            }, 300)
-        }
-    }
-})
-const redButton = document.getElementById("red").addEventListener("click", function () {
-    if (ready) {
-        playerChoice.push(4)
-        playerWin()
-        redFlash()
-        if(!win) {
-            setTimeout(() => {
-                clearColor
-            }, 300)
-        }
-    }
-})
-
 playerWin = () => {
-    if (playerChoice = [playerChoice.length - 1] !== currentGame[playerChoice.length -1]) winning === false
+    // if player was wrong
+    if ([playerChoice.length - 1] !== currentGame[playerChoice.length -1]) {
+        playerChoice = []
+        console.log("check here", playerChoice)
+        winning = false
+        scoreNumber.innerHTML = '0'
+        return
+    }
+
+    // if here, player was right
+
+    scoreNumber.innerHTML = turn
+
     if(playerChoice.length === 10 && winning === true) {
         winGame()
+        return
     }
-    if (win === false) {
-        flashColor()
-        setTimeout(() => {
-            scoreCounter.innerHTML = 0
-            clearColor()
-        }, 1000)
-    }
+
+    // flashes the next turn
+    setTimeout(() => {
+        turn++
+        flashTurn()
+        winning = true
+    }, 1000)
 }
 
 
 
-flashColor = () => {
+blueButton.addEventListener("click", function() {
+    playerChoice.push(1)
+    blueFlash()
+    setTimeout(() => {
+        clearColor()
+        playerWin()
+    }, 300)
+})
+
+greenButton.addEventListener("click", function(){
+    playerChoice.push(2)
+    greenFlash()
+    setTimeout(() => {
+        clearColor()
+        playerWin()
+    }, 300)
+})
+
+yellowButton.addEventListener("click", function() {
+    playerChoice.push(3)
+    yellowFlash()
+    setTimeout(() => {
+        clearColor()
+        playerWin()
+    }, 300)
+})
+
+redButton.addEventListener("click", function () {
+    playerChoice.push(4)
+    redFlash()
+    setTimeout(() => {
+        clearColor()
+        playerWin()
+    }, 300)
+})
+
+flashColor2 = () => {
     blue.style.backgroundColor = "rgb(68, 85, 230)"
     green.style.backgroundColor= "rgb(62, 221, 75)"
     yellow.style.backgroundColor = "rgba(255, 234, 55, 0.8)"
     red.style.backgroundColor = "rgba(221, 75, 62, 0.8)"
 }
 winGame = () => {
-    flashColor()
-    scoreCounter.innerHTML = "YOU WIN!"
+    flashColor2()
+    scoreNumber.innerHTML = "YOU WIN!"
 }
 
 
